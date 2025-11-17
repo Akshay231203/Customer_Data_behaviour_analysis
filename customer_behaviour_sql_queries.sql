7f11399fc743af987fdd7f5252a6dc438e857b5e
Akshay Pulusu <150455519+Akshay231203@users.noreply.github.com>
2025-11-17 23:13:18 +0530

    Add files via upload

---
+++ customer_behaviour_sql_queries.sql
@@ -0,0 +1,78 @@
+
+
+select gender, SUM(purchase_amount) as revenue
+from customer
+group by gender
+
+
+select customer_id, purchase_amount 
+from customer 
+where discount_applied = 'Yes' and purchase_amount >= (select AVG(purchase_amount) from customer)
+
+
+select item_purchased, round(avg(review_rating::numeric),2) as "Average Product Rating"
+from customer
+group by item_purchased
+order by avg(review_rating) desc
+limit 5
+
+select shipping_type, 
+ROUND(AVG(purchase_amount),2)
+from customer
+where shipping_type in ('Standard','Express')
+group by shipping_type;
+
+--Q5. Do subscribed customers spend more? Compare average spend and total revenue 
+--between subscribers and non-subscribers.
+SELECT subscription_status,
+       COUNT(customer_id) AS total_customers,
+       ROUND(AVG(purchase_amount),2) AS avg_spend,
+       ROUND(SUM(purchase_amount),2) AS total_revenue
+FROM customer
+GROUP BY subscription_status
+ORDER BY total_revenue,avg_spend DESC;
+
+select item_purchased,
+       ROUND(100.0 * SUM(CASE WHEN discount_applied = 'Yes' THEN 1 ELSE 0 END)/COUNT(*),2) AS discount_rate
+from customer
+group by item_purchased
+order by discount_rate DESC
+limit 5;
+
+
+with customer_type as (
+SELECT customer_id, previous_purchases,
+CASE 
+    WHEN previous_purchases = 1 THEN 'New'
+    WHEN previous_purchases BETWEEN 2 AND 10 THEN 'Returning'
+    ELSE 'Loyal'
+    END AS customer_segment
+FROM customer)
+
+select customer_segment,count(*) AS "Number of Customers" 
+from customer_type 
+group by customer_segment;
+
+WITH item_counts AS (
+    SELECT category,
+           item_purchased,
+           COUNT(customer_id) AS total_orders,
+           ROW_NUMBER() OVER (PARTITION BY category ORDER BY COUNT(customer_id) DESC) AS item_rank
+    FROM customer
+    GROUP BY category, item_purchased
+)
+SELECT item_rank,category, item_purchased, total_orders
+FROM item_counts
+WHERE item_rank <=3;
+ 
+select subscription_status, count(customer_id) as repeat_buyers
+from customer
+where previous_purchases > 5
+group by subscription_status;
+
+select
+    age_group,
+    sum(purchase_amount) as total_revenue
+from customer
+group by age_group
+order by total_revenue desc;
